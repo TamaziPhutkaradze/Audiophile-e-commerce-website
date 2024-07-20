797da4dc6bdab17e21d27dc374a7bfb4cb9bf8ed
achiPhutkaradze <achifutkaradze9@gmail.com>
2024-07-20 23:42:36 +0400

    first

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Content from "./components/Content";
+import Header from "./components/Header";
+function App() {
+  return (
+    <>
+      <Header />
+      <Content />
+    </>
+  );
+}
+
+export default App;
