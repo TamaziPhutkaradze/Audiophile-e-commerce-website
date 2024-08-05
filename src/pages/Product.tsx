8a4d20cfd06011a8414b8af5418a3e8e40772679
achiPhutkaradze <achifutkaradze9@gmail.com>
2024-08-05 23:31:09 +0400

    feat: add product pages main functionality

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,3 +1,11 @@
+import data from "../../public/data.json";
 export default function Product() {
+  console.log(data);
+  const productName = localStorage.getItem("product name");
+  console.log(productName);
+  const filteredData = data.filter((item) => {
+    return item.name === productName;
+  });
+  console.log(filteredData);
   return <div>Product</div>;
 }
